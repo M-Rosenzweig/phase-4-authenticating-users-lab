ca80e92e36571d5c83e6ca57f152386fcba5f6bd
Moshe <moisherosenzweig@gmail.com>
2022-04-11 14:34:21 -0400

    commit tree 5be950b1ebaa0afab25c1562fa05a00dd67ab54f

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,23 @@
+class SessionsController < ApplicationController
+  
+    def create
+      user = User.find_by(username: params[:username])
+      session[:user_id] = user.id
+      render json: user
+    end
+  
+    def destroy
+      session.delete :user_id
+      head :no_content
+    end
+  
+end
+
+
+# class SessionsController < ApplicationController
+#     def index
+#         session[:session_hello] ||= "World"
+#         cookies[:cookies_hello] ||= "World"
+#         render json: { session: session, cookies: cookies.to_hash }
+#       end
+# end
